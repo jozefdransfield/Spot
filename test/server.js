3c7eef9800ad4b44fd521c281f1e8dcfccfd45b1
Jozef Dransfield <jozefdransfield@gmail.com>
2011-09-13 16:18:51 +0000

    Started work on uploading files

--- test/server.js
+++ test/server.js
@@ -1,4 +1,43 @@
-var app = require("express");
+var express = require("express");
+var form = require('connect-form');
+console.log(form);
+
+var app = express.createServer();
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(form({ keepExtensions: true}))
+
+    app.use(app.router);
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+
+    app.use(express.session({ secret: 'keyboard cat' }));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 
-app.listen(8080);
+app.get("/respondJson", function(req, res) {
+   res.send({response: "success", headers: req.headers}); 
+});
+
+app.put("/respondJson", function(req, res) {
+   res.send({response: "success", headers: req.headers}); 
+});
+
+app.delete("/respondJson", function(req, res) {
+   res.send({response: "success", headers: req.headers}); 
+});
+
+app.post("/respondJson", function(req, res) {
+   res.send({response: "success", headers: req.headers}); 
+});
+
+app.post("/fileupload", function(req, res) {
+   req.form.complete(function(err, fields, files) {
+      res.send({response: "success"}); 
+   });
+});
+
+app.listen(process.env.C9_PORT);
--- test/spot.js
+++ test/spot.js
@@ -7,14 +7,93 @@ require("./server.js");
 var suite = vows.describe('Spot');
 
 suite.addBatch({
-    'Get www.google.com': {
+    'Getting a url that returns JSON': {
         topic: function() {
-			spot.get("http://www.google.co.uk").send(this.callback);
+			spot.get(resource("/respondJson")).send(this.callback);
         },
-        "returns 200": function(error, data) {
-			assert.equal(data.status, "200");
-		}
+        "returns successfully": successfulResponse
+    },
+    'Posting a url that returns JSON': {
+        topic: function() {
+            spot.post(resource("/respondJson")).send(this.callback);
+        },
+        "returns successfully": successfulResponse
+    },
+    'Deleting a url that returns JSON': {
+        topic: function() {
+            spot.delete(resource("/respondJson")).send(this.callback);
+        },
+        "returns successfully": successfulResponse
+    },
+    'Putting a url that returns JSON': {
+        topic: function() {
+            spot.put(resource("/respondJson")).send(this.callback);
+        },
+        "returns successfully": successfulResponse
+    },
+    'Getting a url with headers that returns JSON ': {
+        topic: function() {
+            spot.get(resource("/respondJson")).header("jack", "jill").header("bill", "ben").send(this.callback);
+        },
+        "returns successfully": successfulResponse,
+        "has headers": function(error, response) {
+            assert.equal(response.data.headers.jack, "jill");    
+            assert.equal(response.data.headers.bill, "ben");
+        }
+    },
+    'Putting a url with headers that returns JSON ': {
+        topic: function() {
+            spot.put(resource("/respondJson")).header("jack", "jill").header("bill", "ben").send(this.callback);
+        },
+        "returns successfully": successfulResponse,
+        "has headers": function(error, response) {
+            assert.equal(response.data.headers.jack, "jill");    
+            assert.equal(response.data.headers.bill, "ben");
+        }
+    },
+    'Deleting a url with headers that returns JSON ': {
+        topic: function() {
+            spot.delete(resource("/respondJson")).header("jack", "jill").header("bill", "ben").send(this.callback);
+        },
+        "returns successfully": successfulResponse,
+        "has headers": function(error, response) {
+            assert.equal(response.data.headers.jack, "jill");    
+            assert.equal(response.data.headers.bill, "ben");
+        }
+    },
+    'Posting a url with headers that returns JSON ': {
+        topic: function() {
+            spot.post(resource("/respondJson")).header("jack", "jill").header("bill", "ben").send(this.callback);
+        },
+        "returns successfully": successfulResponse,
+        "has headers": function(error, response) {
+            assert.equal(response.data.headers.jack, "jill");    
+            assert.equal(response.data.headers.bill, "ben");
+        }
+    }
+});
+
+// Havnt got tests for sending content
+
+suite.addBatch({
+    'Post with file': {
+        topic: function() {
+            spot.post("http://cgi-lib.berkeley.edu/ex/fup.cgi").file("./server.js").send(this.callback);
+        },
+        "returns successfully": successfulResponse,
+        "meh": function(err, response) {
+               console.log(response);
+        }
     }
 });
 
 suite.export(module);
+
+function resource(path) {
+    return "http://0.0.0.0:"+process.env.C9_PORT+path;
+}
+
+function successfulResponse(error, response) {
+  assert.equal(response.status, "200");
+  assert.equal(response.data.response, "success" );
+}
